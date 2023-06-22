e0b3eb0b95ea40adef900283c161711c2735e561
Santiago Mauhourat <smauhourat@aadim.org.ar>
2023-06-22 18:38:42 -0300

    contact form

---
+++ assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,17 @@
+<?php
+    class PHP_Email_Form
+    {
+        public string to;
+        public string from_name;
+        public string from_email;
+        public string subject;
+        //public string smtp;
+        public string message;
+
+        public function send()
+        {
+            return "Ok"
+        }
+
+    }
+?>
--- forms/contact.php
+++ forms/contact.php
@@ -6,36 +6,41 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  echo "OK"
+  return;
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  //   $receiving_email_address = 'info@tilceny.com';
+
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
+
+
+
+  // $contact = new PHP_Email_Form;
+  // //$contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  
+  // $contact->smtp = array(
+  //   'host' => 'server377.dinamichosting.com',
+  //   'username' => 'webmaster@tilceny.uy',
+  //   'password' => 'Tacho555_',
+  //   'port' => '465'
+  // );
+  
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  //$contact->add_message( $_POST['name'], 'From');
+  //$contact->add_message( $_POST['email'], 'Email');
+  //$contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  //echo $contact->send();
 ?>
