3e5624e768903dec4cb34febc0c631d877ae3b6f
Santiago Mauhourat <smauhourat@aadim.org.ar>
2023-06-23 13:23:54 -0300

    contact and texts

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -1,18 +1,30 @@
 <?php
-    // class PHP_Email_Form
-    // {
-    //     public string to;
-    //     public string from_name;
-    //     public string from_email;
-    //     public string subject;
-    //     //public string smtp;
-    //     public string message;
 
-    //     public function send()
-    //     {
-    //         return "Ok"
-    //     }
+    class PHP_Email_Form
+    {
+        public $to = '';
+        public $from_name = '';
+        public $from_email = '';
+        public $subject = '';
+        public $message = '';
+        public $smtp_host = '';
+        public $smtp_username = '';
+        public $smtp_password = '';
+        public $smtp_port = '';
 
-    // }
-    echo "caca";
+        public function send()
+        {
+            echo "call send()\n";
+            echo "to: $this->to \n";
+            echo "from_name: $this->from_name \n";
+            echo "from_email: $this->from_email \n";
+            echo "subject: $this->subject \n";
+            echo "message: $this->message \n";
+            echo "smtp_host: $this->smtp_host \n";
+            echo "smtp_username: $this->smtp_username \n";
+            echo "smtp_password: $this->smtp_password \n";
+            echo "smtp_port: $this->smtp_port \n";
+        }
+
+    }
 ?>
--- forms/contact.php
+++ forms/contact.php
@@ -1,57 +1,102 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+  require("class.phpmailer.php");
+  require("class.smtp.php");
 
-
+  echo "------------------ ENVIADOS ------------------ \n";
   if(isset($_POST["name"]))
-        echo $_POST["name"];
-  echo "\n";
+        echo $_POST["name"] . "\n";
   if(isset($_POST["email"]))
-        echo $_POST["email"];
-        echo "\n";
+        echo $_POST["email"] . "\n";
   if(isset($_POST["subject"]))
-        echo $_POST["subject"];
-        echo "\n";
+        echo $_POST["subject"] . "\n";
   if(isset($_POST["message"]))
-        echo $_POST["message"];
-  echo "\n";
+        echo $_POST["message"] . "\n";
 
-  $receiving_email_address = 'info@tilceny.com';
-  echo $receiving_email_address;
-  echo "\n";
-  echo "FIN";
-  echo "\n";
+  // $receiving_email_address = 'info@tilceny.com';
+  // echo $receiving_email_address . "\n";
+  echo "------------------ FIN ------------------ \n";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    echo $php_email_form;
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
 
-  echo "FIN2";
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 465; 
+$mail->SMTPSecure = 'ssl';
+$mail->IsHTML(true); 
+$mail->CharSet = "utf-8";
 
-  // $contact = new PHP_Email_Form;
-  // //$contact->ajax = true;
+// VALORES A MODIFICAR //
+$mail->Host = 'server377.dinamichosting.com'; 
+$mail->Username = 'webmaster@tilceny.uy'; 
+$mail->Password = 'Tacho555_';
+
+$mail->From = 'webmaster@tilceny.com'; // Email desde donde envío el correo.
+$mail->FromName = 'Contacto-Web';
+$mail->AddAddress('info@tilceny.com'); // Esta es la dirección a donde enviamos los datos del formulario
+
+$mail->Subject = "Tilceny.uy - Contacto Web"; // Este es el titulo del email.
+$Body = "";
+$Body .= "Name: ";
+$Body .= $_POST["name"];
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $_POST["email"];
+$Body .= "\n";
+$Body .= "Subject: ";
+$Body .= $_POST["subject"];
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $_POST["message"];
+$Body .= "\n";
+$mensajeHtml = nl2br($Body);
+$mail->Body = "{$mensajeHtml} <br /><br />{$name}<br />{$email}"; // Texto del email en formato HTML
+$mail->AltBody = "{$Body} \n\n"; // Texto sin formato <HTML></HTML>
+
+
+// FIN - VALORES A MODIFICAR //
+
+$success = $mail->Send(); 
+
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "OK";
+}else{
+    if($errorMSG == ""){
+        echo "Ha ocurrido un error :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+
+  // echo "------------------ RECIBIDOS ------------------ \n";
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   echo $php_email_form . "\n";
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
   
+  // $contact = new PHP_Email_Form;
+  // // //$contact->ajax = true;
   // $contact->to = $receiving_email_address;
   // $contact->from_name = $_POST['name'];
   // $contact->from_email = $_POST['email'];
   // $contact->subject = $_POST['subject'];
+  // $contact->message = $_POST['message'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
-  // $contact->smtp = array(
-  //   'host' => 'server377.dinamichosting.com',
-  //   'username' => 'webmaster@tilceny.uy',
-  //   'password' => 'Tacho555_',
-  //   'port' => '465'
-  // );
-  
+  // $contact->smtp_host = 'server377.dinamichosting.com';
+  // $contact->smtp_username = 'webmaster@tilceny.uy';
+  // $contact->smtp_password = 'Tacho555_';
+  // $contact->smtp_port = '465';
+
+  // $contact->send();
+  // echo "------------------ FIN ------------------ \n";
+
+
+
+ 
 
   //$contact->add_message( $_POST['name'], 'From');
   //$contact->add_message( $_POST['email'], 'Email');
